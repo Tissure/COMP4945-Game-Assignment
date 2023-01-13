8f82d8cd6c051b57b9dfbd8388b168dcd93cd43f
Tissure <kyle.ng7512@gmail.com>
2023-01-13 14:55:02 -0800

    Add Multicast.c to namespace NetworkModule

--- NetworkGame/Assets/Network/UDP/Multicast.cs
+++ NetworkGame/Assets/Network/UDP/Multicast.cs
@@ -6,116 +6,113 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
-public class Multicast {
-    private static IPAddress mcastAddress;
-    private static int mcastPort;
-    private static Socket mcastSocket;
-    private static MulticastOption mcastOption;
-    private static IPAddress localIP;
-    private static EndPoint localEP;
-    private static IPEndPoint groupEP;
-    private static EndPoint remoteEP;
+namespace NetworkModule {
+    public class Multicast {
+        private static IPAddress mcastAddress;
+        private static int mcastPort;
+        private static Socket mcastSocket;
+        private static MulticastOption mcastOption;
+        private static IPAddress localIP;
+        private static EndPoint localEP;
+        private static IPEndPoint groupEP;
+        private static EndPoint remoteEP;
 
-    /// <summary>
-    /// Initializes network with default IP and port number
-    /// </summary>
-    public void initDefaultNetwork() {
-        // Create endpoint
+        /// <summary>
+        /// Initializes network with default IP and port number
+        /// </summary>
+        public void initDefaultNetwork() {
+            // Create endpoint
 
-        // Initialize the multicast address group and multicast port.
-        // Both address and port are selected from the allowed sets as
-        // defined in the related RFC documents. These are the same 
-        // as the values used by the sender.
+            // Initialize the multicast address group and multicast port.
+            // Both address and port are selected from the allowed sets as
+            // defined in the related RFC documents. These are the same 
+            // as the values used by the sender.
 
-        // Multicast Address that the reciever will 'subscribe' to
-        mcastAddress = IPAddress.Parse("230.0.0.1");
+            // Multicast Address that the reciever will 'subscribe' to
+            mcastAddress = IPAddress.Parse("230.0.0.1");
 
-        // Multicast Port
-        mcastPort = 11000;
+            // Multicast Port
+            mcastPort = 11000;
 
-        try {
-            // Set local IPaddress to Any
-            localIP = IPAddress.Any;
+            try {
+                // Set local IPaddress to Any
+                localIP = IPAddress.Any;
 
-            // Create new Socket, defining the AddressFamily, SocketType, and ProtocolType
-            mcastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                // Create new Socket, defining the AddressFamily, SocketType, and ProtocolType
+                mcastSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-            // Endpoint to Bind to
-            localEP = (EndPoint) new IPEndPoint(localIP, mcastPort);
-         
-            // Set socket option to reuse address
-            mcastSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
+                // Endpoint to Bind to
+                localEP = (EndPoint) new IPEndPoint(localIP, mcastPort);
 
-            // Bind to IPEndpoint
-            mcastSocket.Bind(localEP);
+                // Set socket option to reuse address
+                mcastSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1);
 
-            Thread receivingThread = new Thread(Receive);
-            receivingThread.IsBackground = true;
-            receivingThread.Start();
+                // Bind to IPEndpoint
+                mcastSocket.Bind(localEP);
 
-            // Create Thread to listen for incomming messages
-        } catch (Exception e) {
-            Console.WriteLine(e.ToString());
+                Thread receivingThread = new Thread(Receive);
+                receivingThread.IsBackground = true;
+                receivingThread.Start();
+
+                // Create Thread to listen for incomming messages
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
         }
+
+        /// <summary>
+        /// Sends Yeet over socket connection
+        /// </summary>
+        /// <returns>
+        /// <c>True</c> if messgae successfully sends.
+        /// <c>False</c> otherwise
+        /// </returns>
+        public bool Send() {
+            //if (socket == null) {
+            //    System.Diagnostics.Debug.WriteLine("Network not initialized");
+            //    return false;
+            //}
+            //string msg = "Yeet";
+            //System.Diagnostics.Debug.WriteLine("Message sent: " + msg);
+            //return true;
+
+            try {
+
+                mcastSocket.SendTo(ASCIIEncoding.ASCII.GetBytes("Hello Multicast Listener"), groupEP);
+                Console.WriteLine("Multicast data sent.....");
+                return true;
+            } catch (Exception e) {
+                Console.WriteLine("\n" + e.ToString());
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Recieves Payload from Socket Connection Subscribed to Sender. 
+        /// Delegate method: To be run in a background thread, to 'receive' constantly.
+        /// </summary>
+        public void Receive() {
+            // Create Multicast Option to set later
+            mcastOption = new MulticastOption(mcastAddress, localIP);
+
+            // Set socket options. Subscribe (Add Membership) to Multicast Broadcast
+            mcastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, mcastOption);
+
+            byte[] message = new byte[1024];
+
+            // Endpoint to recieve message from (Any Ip Address, Port: 0)
+            remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Waiting for packets..");
+            // Recieved bytes
+            int recv = mcastSocket.ReceiveFrom(message, ref remoteEP);
+
+            while (recv != 0) {
+                Console.WriteLine("Recieved packets..\n" + Encoding.ASCII.GetString(message));
+                recv = mcastSocket.ReceiveFrom(message, ref remoteEP);
+            }
+        }
+
     }
-
-    /// <summary>
-    /// Sends Yeet over socket connection
-    /// </summary>
-    /// <returns>
-    /// <c>True</c> if messgae successfully sends.
-    /// <c>False</c> otherwise
-    /// </returns>
-    public bool Send() {
-        //if (socket == null) {
-        //    System.Diagnostics.Debug.WriteLine("Network not initialized");
-        //    return false;
-        //}
-        //string msg = "Yeet";
-        //System.Diagnostics.Debug.WriteLine("Message sent: " + msg);
-        //return true;
-        
-        try
-        {
-            
-            mcastSocket.SendTo(ASCIIEncoding.ASCII.GetBytes("Hello Multicast Listener"), groupEP);
-            Console.WriteLine("Multicast data sent.....");
-            return true;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("\n" + e.ToString());
-            return false;
-        }
-
-    }
-
-    /// <summary>
-    /// Recieves Payload from Socket Connection Subscribed to Sender. 
-    /// Delegate method: To be run in a background thread, to 'receive' constantly.
-    /// </summary>
-    public void Receive()
-    {
-        // Create Multicast Option to set later
-        mcastOption = new MulticastOption(mcastAddress, localIP);
-
-        // Set socket options. Subscribe (Add Membership) to Multicast Broadcast
-        mcastSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, mcastOption);
-
-        byte[] message = new byte[1024];
-
-        // Endpoint to recieve message from (Any Ip Address, Port: 0)
-        remoteEP = new IPEndPoint(IPAddress.Any, 0);
-
-        Console.WriteLine("Waiting for packets..");
-        // Recieved bytes
-        int recv = mcastSocket.ReceiveFrom(message, ref remoteEP);
-
-        while (recv != 0) 
-        {
-            Console.WriteLine("Recieved packets..\n" + Encoding.ASCII.GetString(message));
-            recv = mcastSocket.ReceiveFrom(message, ref remoteEP);
-        }
-    }
-
 }
---
+++ NetworkGame/Assets/Scripts/Preloader.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class Preloader : MonoBehaviour
+{
+    // Awake is called on object initialization
+    void Awake()
+    {
+      
+    }
+}
