a2a109c23dbc6ae74b75dd959d9e6df21a4f4c90
Tissure <kyle.ng7512@gmail.com>
2023-01-13 12:54:25 -0800

    Create Network folder and respective files

---
+++ NetworkGame/Assets/Network/UDP/Multicast.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+public class Multicast {
+    Socket socket = null;
+    IPEndPoint IPEndPoint = null;
+
+    /// <summary>
+    /// Initializes network with default IP and port number
+    /// </summary>
+    public void initDefaultNetwork() {
+        // Create endpoint
+        // Create socket
+        // Bind socket
+        // Create Thread to listen for incomming messages
+    }
+
+    /// <summary>
+    /// Sends Yeet over socket connection
+    /// </summary>
+    /// <returns>
+    /// True if messgae succeeds
+    /// </returns>
+    public bool Send() {
+        if (socket == null) {
+            System.Diagnostics.Debug.WriteLine("Network not initialized");
+            return false;
+        }
+        string msg = "Yeet";
+        System.Diagnostics.Debug.WriteLine("Message sent: " + msg);
+        return true;
+    }
+}
