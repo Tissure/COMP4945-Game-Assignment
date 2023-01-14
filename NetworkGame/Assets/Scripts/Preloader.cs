6dba2ce620dfdb79b30fcc93fca434a3df286fc0
Tissure <kyle.ng7512@gmail.com>
2023-01-13 17:45:34 -0800

    Verify packets are sending

--- NetworkGame/Assets/Scripts/Preloader.cs
+++ NetworkGame/Assets/Scripts/Preloader.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using NetworkModule;
-using UnityEditor.PackageManager;
 using System.Reflection;
 using System.IO;
 using UnityEngine;
