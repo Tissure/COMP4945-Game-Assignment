d05d94ea1fcc96aab237d98bdbe5904a060b8505
JungMin Oh <joh67@my.bcit.ca>
2023-01-16 14:36:31 -0800

    Simple Pong Game Working on Unity Engine

---
+++ NetworkGame/Assets/Scripts/Game/Ball.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    public Vector3 startpos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position;
+        Launch();   
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startpos;
+        Launch();        
+    }
+
+    private void Launch(){
+        // Launch the ball
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.velocity = new Vector2(x * speed, y * speed);
+    }
+}
